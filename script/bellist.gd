3d0361f6ce6b997423383ffae6f4246a65558dc5
JugAnt0 <canaletwomeyantonio@gmail.com>
2025-11-21 18:44:08 -0500

    Added cat movement and custom cursor to Panda

---
+++ script/bellist.gd
@@ -0,0 +1,32 @@
+extends CharacterBody2D
+
+
+
+
+var pointer = preload("res://assets/sprites/pointer.png")
+var cat = preload("res://assets/sprites/Cat-cursor.png")
+
+func _on_bellist_mouse_entered() -> void:
+	Input.set_custom_mouse_cursor(cat, Input.CURSOR_ARROW, Vector2(16,16))
+
+
+func _on_bellist_mouse_exited() -> void:
+	Input.set_custom_mouse_cursor(pointer, Input.CURSOR_ARROW, Vector2(16,16))
+	
+	
+	
+	
+	
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	move_and_slide()
+
+
+func _on_mouse_entered() -> void:
+	Input.set_custom_mouse_cursor(cat, Input.CURSOR_ARROW, Vector2(16,16))
+
+
+func _on_mouse_exited() -> void:
+	Input.set_custom_mouse_cursor(pointer, Input.CURSOR_ARROW, Vector2(16,16))
--- script/indoor_red_house.gd
+++ script/indoor_red_house.gd
@@ -3,3 +3,13 @@ extends Node2D
 
 func _ready() -> void:
 	$fade_transition/AnimationPlayer.play("fade_out")
+
+var pointer = preload("res://assets/sprites/pointer.png")
+var cat = preload("res://assets/sprites/Cat-cursor.png")
+
+func _on_bellist_mouse_entered() -> void:
+	Input.set_custom_mouse_cursor(cat, Input.CURSOR_ARROW, Vector2(16,16))
+
+
+func _on_bellist_mouse_exited() -> void:
+	Input.set_custom_mouse_cursor(pointer, Input.CURSOR_ARROW, Vector2(16,16))
