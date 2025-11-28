4b7aee2f2aac418a5ff203c96c6334ba3e14f3af
JugAnt0 <canaletwomeyantonio@gmail.com>
2025-11-27 21:53:56 -0500

    Began work on new cat 'Flora. added sprite and animations

---
+++ script/flora's_minigame_selection.gd
@@ -0,0 +1,31 @@
+extends Node2D
+
+var button_type = null
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$fade_transition/AnimationPlayer.play("fade_out")
+
+
+func _on_sky_high_pressed() -> void:
+	button_type = "SkyHigh"
+	$fade_transition.show()
+	$fade_transition/FadeTimer.start()
+	$fade_transition/AnimationPlayer.play("fade_in")
+	
+
+
+func _on_go_back_pressed() -> void:
+	button_type = "GoBack"
+	$fade_transition.show()
+	$fade_transition/FadeTimer.start()
+	$fade_transition/AnimationPlayer.play("fade_in")
+	
+
+func _on_fade_timer_timeout():
+	if button_type == "SkyHigh":
+		
+		get_tree().change_scene_to_file("res://scenes/flora's_sky_high.tscn")
+		
+	elif button_type == "GoBack":
+		get_tree().change_scene_to_file("res://scenes/edificio_indoor.tscn")
+	
